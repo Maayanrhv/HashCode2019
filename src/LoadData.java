33076b285051892f5e7b31c65c8893d600e021e4
ShirKempinski <shirk9@walla.co.il>
2019-02-28 19:29:09 +0200

    adding loadData file

---
+++ src/LoadData.java
@@ -0,0 +1,27 @@
+/**
+ * LoadData reads lines from a file to a List<String>.
+ * @param data - the List<String>. 
+ * @param fileName - the path to the file that holds the data
+ */
+private void LoadData(List<String> data, String fileName) {
+    BufferedReader reader = null; 
+    try {
+        // read the first line as a String
+        reader = new BufferedReader(new FileReader(new File(fileName)));
+        String firstLine = reader.readLine();
+        
+        // go over all the records and add them to the records list 
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            data.add(line);
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (reader != null)
+                reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
