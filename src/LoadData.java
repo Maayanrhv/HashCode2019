aaf615a009822b87f3c4fa001d7434bf8e14b1f2
ShirKempinski <shirk9@walla.co.il>
2019-02-28 19:34:40 +0200

    editing loadData file

--- src/LoadData.java
+++ src/LoadData.java
@@ -1,27 +1,35 @@
-/**
- * LoadData reads lines from a file to a List<String>.
- * @param data - the List<String>. 
- * @param fileName - the path to the file that holds the data
- */
-private void LoadData(List<String> data, String fileName) {
-    BufferedReader reader = null; 
-    try {
-        // read the first line as a String
-        reader = new BufferedReader(new FileReader(new File(fileName)));
-        String firstLine = reader.readLine();
-        
-        // go over all the records and add them to the records list 
-        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-            data.add(line);
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    } finally {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class LoadData {
+
+    /**
+     * LoadData reads lines from a file to a List<String>.
+     * @param data - the List<String>. 
+     * @param fileName - the path to the file that holds the data
+     */
+    private void LoadData(List<String> data, String fileName) {
+        BufferedReader reader = null; 
         try {
-            if (reader != null)
-                reader.close();
+            // read the first line as a String
+            reader = new BufferedReader(new FileReader(new File(fileName)));
+            String firstLine = reader.readLine();
+
+            // go over all the records and add them to the records list 
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                data.add(line);
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
